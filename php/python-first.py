54381601a507455dd88f81e4685800d747260dae
rabia-aaziz <39001798+rabia-aaziz@users.noreply.github.com>
2024-10-18 15:05:05 +0500

    Add files via upload

---
+++ php/python-first.py
@@ -0,0 +1,93 @@
+# num1 = input("Enter The first number")
+# num2 = input("Enter The Second number")
+# num3 = input("Enter The Third number")
+#
+# num1 = float(num1)
+# num2 = float(num2)
+# num3 = float(num3)
+#
+#
+# average = num1 + num2 + num3 /3
+#
+# print ("The Average of {0}, {1} and {2} are ")
+
+# ---------------to find square root-------------------------
+# num = input("find the sq root")
+#
+# exp = int(num) ** int(0.5)
+#
+# print("The square root of {0} is1 {1}".format(num,exp))
+
+# import cmath
+# num = 1+2j
+# num_sqrt = cmath.sqrt(num)
+# print ("The Square root of {0} is {1:0.3f} + {2:0.3f}" .format(num,num_sqrt.real,num_sqrt.imag))
+
+# find the area of the triangle
+# sideA = int(input("Side A"))
+# sideB = int(input("Side B"))
+# sideC = int(input("Side C"))
+# side = sideA + sideB + sideC /2
+# area = side * (side-sideA) * (side-sideB) * (side-sideC) ** 0.5
+# print("The area of Triangle side A {0},B {1} and C {2} is {3} sq feet and the square root is {4:0.2f}".format(sideA,sideB,sideC,side,area))
+
+# ----------------------Celcius to fahrenheit---------------------
+# celcius = int(input("enter your temperature"))
+#
+# fahrenheit = (celcius * 1.8) + 32
+# print ("%0.3f celcius degree is equal to %0.3f fahrenheit degree."%(celcius,fahrenheit))
+
+
+# ----------------------fahrenheit to Celcius ---------------------
+# fahrenheit= int(input("Fahrenheit to Celcius"))
+#
+# celcius = (fahrenheit - 32 ) * 1.8
+# print ("%0.3f fahrenheit degree is equal to %0.3f celcius degree."%(celcius,fahrenheit))
+
+
+# ------------------------Temperature converter-----------------------
+# print("Temperature converter")
+# degree = int(input(" enter your temp "))
+# method = (input("Fahrenheit to celcius enter C and celcius to fahrenheit enter F"))
+
+# ----------------fahrenheit to celcius------------
+# if method == "C":
+#     fahrenheit = (degree * 1.8)+32
+#     print ("The celcius of {0} is = {1}".format(degree,fahrenheit))
+
+# --------------------------------------------------celcius to fahrenheit------------------------------------
+
+# elif method == "F":
+#     celcius = (degree - 32)* 1.8
+#     print ("The fahrenheit of {0} is = {1}".format(degree,celcius))
+# -----------------------------------------------------------------------------------------------------------------
+# import cmath
+# a = int(input("value of a"))
+# b = int(input("value of b"))
+# c = int(input("value of c"))
+#
+# root_sq_root= int(b ** 2) - int(4 * a * c)
+#
+# sol1 = (-b+cmath.sqrt(root_sq_root)/2*a)
+#
+# sol2 = (-b-cmath.sqrt(root_sq_root)/2*a)
+#
+# print("Value of a = {0}, b = {1} and c = {2} are equal to X {3} and {4}".format(a,b,c,sol1,sol2))
+#-------------------------------------------------------------------------------------------------------------------
+
+# -----------------Loop-----------------------
+#
+# multiplier = 5
+# counter = 1
+#
+# while counter <= 10:
+#     result = counter * multiplier
+#     print(f"{multiplier} * {counter} = {result}")
+#
+#     counter += 1
+
+
+
+
+
+
---
+++ php/script.js
@@ -0,0 +1,146 @@
+const name = "dua";
+
+var  age = "19";
+ 
+
+
+
+
+
+// const insta = {
+//     name: "sana",
+//     followers: 854,
+//     posts: "216post",
+//     following: "2following",
+//     isfollow: true,
+// };
+
+// console.log(insta)
+
+// let name = "Rabia";
+// let age = 21;
+// let gender = "Female";
+// let languge = "Urdu";
+// let cnic = 4230164060546;
+
+// console.log(name,age,gender,languge,cnic)
+// let maxSpeed = 200;
+// let currentSpeed =120;
+
+// if
+//     (currentSpeed > maxSpeed)
+//     {
+//       console.log("OVER");
+//     }
+    
+//     else
+//     console.log("NOT");
+
+// let myAbsents = 4;
+
+// if (myAbsents < 0){
+//     console.log("INVALID NUMBER. No. of absents cannot be negative");
+// }
+
+// else if (myAbsents <= 2) {
+//     console.log("Congrats! You are eligible for the award");
+// } 
+
+// else {
+//     console.log("Sorry, but you have been absent too many times. You are ineligible for the awards :( ");
+// }
+
+
+
+
+// let a = 2;
+// let b = 5;
+
+// console.log(a==b);
+// console.log(a===b);
+// console.log(a!=b);
+// console.log(a!==b);
+
+
+// console.log("a+=b =", a += b);
+// console.log(a);
+// console.log("a-=b =", a -= b);
+// console.log("a*=b =", a *= b);
+// console.log("a/=b =", a /= b);
+// console.log("a%=b =", a %= b);
+
+// let name = "sana";
+// let isLoggedIn = false;
+
+// if (isLoggedIn === true) {
+//     console.log("Welcome, " + name);
+// } else {
+//     console.log("Please log in");
+// }
+
+
+
+// let num =24;
+// if( num % 3 === 0){
+//     console.log("divisible  by 3");
+// }
+// else{
+//     console.log("not divisible by 3");
+// }
+
+
+// let costOfApple = 15;
+// let costOfMango = 10;
+
+// if (costOfApple == costOfMango) {
+//     console.log('Apple and mango cost are same!');
+// } else if (costOfMango > costOfApple) {
+//     console.log("Mango is more expensive");
+// } else {
+//     console.log("Apple is more expensive");    
+// }
+
+// let num = prompt("multiple of 5 or not");
+// if( num % 5 === 0){
+//     console.log("multiple of 5");
+// }
+// else{
+//     console.log("not multiple of 5");
+// }
+
+
+// let totalMarks = 90;
+// let obtainMarks = 50;
+
+// let totalMarks = prompt("Grade");;
+
+
+// if 
+// (totalMarks >= 100)
+// {
+//     console.log("A");
+// }
+// else if
+// (totalMarks = 70)
+// {
+//     console.log("B");
+// }
+
+// else if
+// (totalMarks = 60)
+// {
+//     console.log("C");
+// }
+
+// else if
+// (totalMarks = 50)
+// {
+//     console.log("D");
+// }
+
+// else 
+// (totalMarks <= 49)
+// {
+//     console.log("FAil");
+// }
+
